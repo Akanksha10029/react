e74d0f9af5fd3936b740166de865a4a8de2b1366
Akanksha10029 <akanksharani405@gmail.com>
2025-02-25 21:44:59 +0530

    changes updated

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react'
+import Trial from './Trial.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    // <div>
+    //   <h1>Hello Vite + React!</h1>
+    //   <p>Count: {count}</p> {/* Render the current count */}
+    //   <button onClick={() => setCount(count + 1)}>Increment</button> {/* Increment the count */}
+    //   <button onClick={() => setCount(count - 1)}>Decrement</button> {/* Decrement
+    //   the count */}
+    // </div>
+    <Trial />
+  )
+}
+
+export default App
