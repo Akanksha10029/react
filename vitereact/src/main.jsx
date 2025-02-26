f8138f194e3af50878a7240e2e4c55b79dfb32a3
Akanksha10029 <akanksharani405@gmail.com>
2025-02-26 17:56:25 +0530

    hooks counter

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx' 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "https://google.com", style: "color: red", target: "_blank" },
+//   "Click me to visit google!"
+// );
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
